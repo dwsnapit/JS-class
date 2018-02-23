8f712ee5102c885c7276c92a71f0ee289890f132
dwsnapit <dwsnapit@gmail.com>
2018-02-23 07:06:59 -0600

    adding examples

---
+++ JavascriptBasics/sum.js
@@ -0,0 +1,7 @@
+
+function sum(){
+var  num1 = document.getElementById("num1").value;
+var num2 = document.getElementById("num2").value
+document.getElementById("h1fun").innerHTML = parseInt(num1) + parseInt(num2);
+alert(parseInt(num1) + parseInt(num2));
+}
