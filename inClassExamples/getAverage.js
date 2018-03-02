14cd700579037b8f7657903f5e7d55c316033a62
dwsnapit <dwsnapit@gmail.com>
2018-03-01 21:14:28 -0600

    ClassExample for parsing a char []

---
+++ inClassExamples/getAverage.js
@@ -0,0 +1,36 @@
+var numbers = "2,3,1,44,5,3,21,3,1"
+var size = numbers.length;
+
+var sum = "";
+for (var cnt = 0; cnt < size;cnt++)
+{
+    console.log( sum += numbers[cnt]);
+}
+
+var temp;
+console.log(temp = numbers.split(","));
+
+
+
+
+
+
+var numbers2;
+var getAverage = function(arr){
+    var size = arr.length;
+    console.log(size);
+    var sum = 0;
+    for (var cnt = 0; cnt < size;cnt++)
+    {
+
+        console.log(cnt+", "+size);
+    }
+    
+    //console.log(sum/size);
+}
+
+getAverage(temp);
+
+
+//numbers2.push(numbers.)
+
